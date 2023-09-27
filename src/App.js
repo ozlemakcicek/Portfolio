72154b9138afcd718c990d721589d1978c51afc0
ozlem <ozlemakcicek83@gmail.com>
2023-09-27 19:21:54 +0200

    test.2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Qualification from "./components/qualifikation/Qualification";
 import Projects from "./components/projects/Projects";
-// import Testimonials from "./components/testimonials/Testimonials";
+import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
